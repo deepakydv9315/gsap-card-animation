75dd2778244b3c5358909575fcf66d946ce9d8ad
Deepak Yadav <deepakydv0001@gmail.com>
2022-02-03 23:21:38 +0530

    card

---
+++ app.js
@@ -0,0 +1,23 @@
+let t1 = gsap.timeline()
+
+console.log("new")
+
+t1.from(".card" ,
+ {
+    stagger: 0.2 ,
+    opacity: 0,
+    x: -30
+ }
+);
+
+t1.from(".avatar img" , {
+    stagger: 0.2,
+    opacity: 0,
+    y: 20
+})
+
+.t1.from(".card-footer" , {
+    stagger:0.2,
+    opacity: 0 ,
+    y:10
+});
